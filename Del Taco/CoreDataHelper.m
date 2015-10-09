db32a30182f71003b6191f0a6b5d55ebee048e78
Rakshit Garg <raks.garg@gmail.com>
2015-10-09 16:42:28 -0700

    fix errors

--- Del Taco/CoreDataHelper.m
+++ Del Taco/CoreDataHelper.m
@@ -16,35 +16,47 @@
 NSString * storeFileName = @"DelTacoModel.sqlite";
 
 #pragma mark - PATHS
--(NSString *) applicationDocumentsDirectory {
+-(NSString *) applicationDocumentsDirectory
+{
     if (debug == 1) {
         NSLog(@"Running %@'%@'", self.class,NSStringFromSelector(_cmd));
     }
     return [NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES) lastObject];
 }
 
--(NSURL *) applicationStoresDirectory {
-    if(debug==1){
+-(NSURL *) applicationStoresDirectory
+{
+    if(debug==1)
+    {
         NSLog(@"Running %@'%@'", self.class,NSStringFromSelector(_cmd));
     }
     NSURL *storesDirectory = [[NSURL fileURLWithPath:[self applicationDocumentsDirectory]]
                          URLByAppendingPathComponent:@"DelTaco"];
      NSFileManager *fileManager = [NSFileManager defaultManager];
-     if (![fileManager fileExistsAtPath:[storesDirectory path]]) {
+     if (![fileManager fileExistsAtPath:[storesDirectory path]])
+     {
          NSError *error = nil;
          if ([fileManager createDirectoryAtURL:storesDirectory
                    withIntermediateDirectories:YES
                                     attributes:nil
-                                         error:&error]) {
-             if (debug==1) {
-                 NSLog(@"Successfully created DelTaco directory");}
+                                         error:&error])
+         {
+             if (debug==1)
+             {
+                 NSLog(@"Successfully created DelTaco directory");
+             }
          }
-         else {NSLog(@"FAILED to create Stores directory: %@", error);}
+         else
+         {
+             NSLog(@"FAILED to create Stores directory: %@", error);
+         }
      }
      return storesDirectory;
 }
-- (NSURL *)storeURL {
-    if (debug==1) {
+- (NSURL *)storeURL
+{
+    if (debug==1)
+    {
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
     return [[self applicationStoresDirectory]
@@ -52,12 +64,17 @@ NSString * storeFileName = @"DelTacoModel.sqlite";
 }
 
 #pragma mark - SETUP
-- (id)init {
-    if (debug==1) {
+- (id)init
+{
+    if (debug==1)
+    {
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
     self = [super init];
-    if (!self) {return nil;}
+    if (!self)
+    {
+        return nil;
+    }
     
     _model = [NSManagedObjectModel mergedModelFromBundles:nil];
     _coordinator = [[NSPersistentStoreCoordinator alloc]
@@ -67,38 +84,61 @@ NSString * storeFileName = @"DelTacoModel.sqlite";
     [_context setPersistentStoreCoordinator:_coordinator];
     return self;
 }
-- (void)loadStore {
-    if (debug==1) {
+- (void)loadStore
+{
+    if (debug==1)
+    {
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
-    if (_store) {return;} // Don't load store if it's already loaded
+    if (_store)
+    {
+        return;
+    } // Don't load store if it's already loaded
     NSError *error = nil;
     _store = [_coordinator addPersistentStoreWithType:NSSQLiteStoreType
                                         configuration:nil
                                                   URL:[self storeURL]
                                               options:nil error:&error];
-    if (!_store) {NSLog(@"Failed to add store. Error: %@", error);abort();}
-    else         {if (debug==1) {NSLog(@"Successfully added store: %@", _store);}}
+    if (!_store)
+    {
+        NSLog(@"Failed to add store. Error: %@", error);abort();
+    }
+    else
+    {
+        if (debug==1)
+        {
+            NSLog(@"Successfully added store: %@", _store);
+        }
+    }
 }
 - (void)setupCoreData {
-    if (debug==1) {
+    if (debug==1)
+    {
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
     [self loadStore];
 }
 #pragma mark - SAVING
-- (void)saveContext {
-    if (debug==1) {
+- (void)saveContext
+{
+    if (debug==1)
+    {
         NSLog(@"Running %@ '%@'", self.class, NSStringFromSelector(_cmd));
     }
-    if ([_context hasChanges]) {
+    if ([_context hasChanges])
+    {
         NSError *error = nil;
-        if ([_context save:&error]) {
+        if ([_context save:&error])
+        {
             NSLog(@"_context SAVED changes to persistent store");
-        } else {
+        }
+        else
+        {
             NSLog(@"Failed to save _context: %@", error);
         }
-    } else {
+    }
+    else
+    {
         NSLog(@"SKIPPED _context save, there are no changes!");
     }
 }
